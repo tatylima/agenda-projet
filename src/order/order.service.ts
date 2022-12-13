aa77d541b64a65f297cf836e035f7e8344b6f7f2
tatylima <tatianagandra78@gmail.com>
2022-12-12 23:14:33 -0300

    feat: implements ordem service

--- src/order/order.service.ts
+++ src/order/order.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@nestjs/common';
+import { Prisma } from '@prisma/client';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { handleError } from 'src/utils/handle-error.util';
 import { CreateOrderDto } from './dto/create-order.dto';
 
 @Injectable()
@@ -7,14 +9,76 @@ export class OrderService {
   constructor(private readonly prisma: PrismaService) {}
 
   create(createOrderDto: CreateOrderDto) {
-    return 'This action adds a new order';
+    const data: Prisma.OrderCreateInput = {
+      user: {
+        connect: {
+          id: createOrderDto.userId,
+        },
+      },
+      products: {
+        connect: createOrderDto.products.map((productId) => ({
+          id: productId,
+        })),
+      },
+    };
+
+    return this.prisma.order
+      .create({
+        data,
+        select: {
+          id: true,
+          user: {
+            select: {
+              name: true,
+            },
+          },
+          products: {
+            select: {
+              name: true,
+            },
+          },
+        },
+      })
+      .catch(handleError);
   }
 
   findAll() {
-    return `This action returns all order`;
+    return this.prisma.order.findMany({
+      select: {
+        id: true,
+        user: {
+          select: {
+            name: true,
+          },
+        },
+        _count: {
+          select: {
+            products: true,
+          },
+        },
+      },
+    });
   }
 
   findOne(id: string) {
-    return `This action returns a #${id} order`;
+    return this.prisma.order.findUnique({
+      where: { id },
+      include: {
+        user: {
+          select: {
+            name: true,
+          },
+        },
+        products: {
+          select: {
+            id: true,
+            name: true,
+            price: true,
+            image: true,
+            description: true,
+          },
+        },
+      },
+    });
   }
 }

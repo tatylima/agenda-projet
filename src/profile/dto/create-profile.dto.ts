d4666c812323ff1152e5bee056fc08107c1d8976
tatylima <tatianagandra78@gmail.com>
2022-12-13 12:03:23 -0300

    fix(profile): implement userId (admin) in create profile

--- src/profile/dto/create-profile.dto.ts
+++ src/profile/dto/create-profile.dto.ts
@@ -1,1 +1,27 @@
-export class CreateProfileDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsString, IsUrl } from 'class-validator';
+
+export class CreateProfileDto {
+  @IsString()
+  @ApiProperty({
+    description: 'O nome usado para o perfil',
+    example: 'TatianaGandra123',
+  })
+  name: string;
+
+  @IsString()
+  @ApiProperty({
+    description: 'ID do Usuário (admin)',
+    example: 'f7f45b19-bb7c-4f0d-87ea-f0d3c04423b5',
+  })
+  userId: string;
+
+  @IsUrl()
+  @ApiProperty({
+    description: 'Imagem do Perfil',
+    example: 'https://avatars.githubusercontent.com/u/97922515?v=4',
+  })
+  image: string;
+
+  productId?: string;
+}

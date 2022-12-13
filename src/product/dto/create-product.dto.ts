75d5ce67ec26500603c627ad2910c3b84dc7fd3a
tatylima <tatianagandra78@gmail.com>
2022-12-12 22:30:04 -0300

    feat: implements products

--- src/product/dto/create-product.dto.ts
+++ src/product/dto/create-product.dto.ts
@@ -1,1 +1,35 @@
-export class CreateProductDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNumber, IsString, IsUrl } from 'class-validator';
+
+export class CreateProductDto {
+  @IsString()
+  @ApiProperty({
+    description: 'Nome do produto',
+    example: 'Agenda de Natal',
+  })
+  name: string;
+
+  @IsString()
+  @ApiProperty({
+    description: 'Descrição do produto',
+    example:
+      'Caderno de Anotação',
+  })
+  description: string;
+
+  @IsNumber({
+    maxDecimalPlaces: 2,
+  })
+  @ApiProperty({
+    description: 'Preço do produto',
+    example: 12.34,
+  })
+  price: number;
+
+  @IsUrl()
+  @ApiProperty({
+    description: 'Imagem do produto',
+    example: 'https://i.imgur.com/hNE75Iw.png',
+  })
+  image: string;
+}
